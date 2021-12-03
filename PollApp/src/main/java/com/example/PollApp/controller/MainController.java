88047c69884265809c5ed4661c9ebcb3a15845a8
Hári Gábor <gabor.hari@ecobit.hu>
2021-12-03 19:21:03 +0100

    A model-t is frissítettem az új logika szerint.

--- PollApp/src/main/java/com/example/PollApp/controller/MainController.java
+++ PollApp/src/main/java/com/example/PollApp/controller/MainController.java
@@ -1,7 +1,6 @@
 package com.example.PollApp.controller;
 
 import com.example.PollApp.model.*;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,12 +10,14 @@ import java.util.List;
 public class MainController {
 
     private final AppUserRepository appUserRepository;
+    private final UserRoleRepository userRoleRepository;
     private final QuestionRepository questionRepository;
     private final AnswerRepository answerRepository;
     private final VoteRepository voteRepository;
 
-    public MainController(AppUserRepository appUserRepository, QuestionRepository questionRepository, AnswerRepository answerRepository, VoteRepository voteRepository) {
+    public MainController(AppUserRepository appUserRepository, UserRoleRepository userRoleRepository ,QuestionRepository questionRepository, AnswerRepository answerRepository, VoteRepository voteRepository) {
         this.appUserRepository = appUserRepository;
+        this.userRoleRepository = userRoleRepository;
         this.questionRepository = questionRepository;
         this.answerRepository = answerRepository;
         this.voteRepository = voteRepository;
@@ -25,6 +26,11 @@ public class MainController {
     @GetMapping("/testAppUser")
     public @ResponseBody Iterable<AppUser> listAllUsers() {
         return appUserRepository.findAll();
+    }
+
+    @GetMapping("/testUserRole")
+    public @ResponseBody Iterable<UserRole> listAllRoles() {
+        return userRoleRepository.findAll();
     }
 
     @GetMapping("/testQuestion")
