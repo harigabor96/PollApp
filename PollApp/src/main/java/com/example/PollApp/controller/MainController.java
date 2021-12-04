9b69663548bcfe04b6fb5342840f88dfe01974ba
Hári Gábor <gabor.hari@ecobit.hu>
2021-12-04 23:43:36 +0100

    Login és regisztráció kész

--- PollApp/src/main/java/com/example/PollApp/controller/MainController.java
+++ PollApp/src/main/java/com/example/PollApp/controller/MainController.java
@@ -53,33 +53,46 @@ public class MainController {
         return voteRepository.findAll();
     }
 
-    @PostMapping("/signInRegister")
-    public Object login(@ModelAttribute(name="currentUser") AppUser currentUser,
-                        @RequestParam(value ="action") String action,
-                        RedirectAttributes redirectAttributes) {
-        if (action.equals("signin")) {
-
+    @PostMapping("/login/signIn")
+    public String signIn(@ModelAttribute(name="currentUser") AppUser currentUser,
+                         RedirectAttributes redirectAttributes) {
+        AppUser user = appUserRepository.findAppUserByUsername(currentUser.getUsername());
+        if ( user == null) {
+            redirectAttributes.addFlashAttribute("errorMsg", "Wrong username!");
+            return "redirect:/login";
         }
-        if (action.equals("register")) {
-            if (appUserRepository.findAppUserByUsername(currentUser.getUsername()) != null) {
-                redirectAttributes.addFlashAttribute("errorMsg", "User already exists!");
-                return "redirect:/login";
-            }
-            try {
-                appUserRepository.save(currentUser);
-                //session.setattribute();
-                //return "redirect:/pollList";
-            }
-            catch (Exception e) {
-                if (e instanceof ConstraintViolationException) {
-                    redirectAttributes.addFlashAttribute("errorMsg", printErrors((ConstraintViolationException)e));
-                    return "redirect:/login";
-                }
-                redirectAttributes.addFlashAttribute("errorMsg", "Something went wrong!");
-                return "redirect:/login";
-            }
+        if(user.getPassword().equals(currentUser.getPassword())) {
+            System.out.println("A bejelentkezés sikeres!");
+            //session.setuser
+            //return "redirect:/pollList"
+            return "redirect:/login";
         }
+        redirectAttributes.addFlashAttribute("errorMsg", "Wrong password!");
         return "redirect:/login";
+    }
+
+    @PostMapping("/login/register")
+    public String register(@ModelAttribute(name="currentUser") AppUser currentUser,
+                           RedirectAttributes redirectAttributes) {
+        if (appUserRepository.findAppUserByUsername(currentUser.getUsername()) != null) {
+            redirectAttributes.addFlashAttribute("errorMsg", "User already exists!");
+            return "redirect:/login";
+        }
+        try {
+            appUserRepository.save(currentUser);
+            System.out.println("A regisztráció sikeres!");
+            //session.setuser
+            //return "redirect:/pollList";
+            return "redirect:/login";
+        }
+        catch (Exception e) {
+            if (e instanceof ConstraintViolationException) {
+                redirectAttributes.addFlashAttribute("errorMsg", printErrors((ConstraintViolationException)e));
+                return "redirect:/login";
+            }
+            redirectAttributes.addFlashAttribute("errorMsg", "Something went wrong!");
+            return "redirect:/login";
+        }
     }
 
     @GetMapping("/login")
@@ -87,15 +100,16 @@ public class MainController {
         List<UserRole> userRoles = userRoleRepository.findAll();
         model.addAttribute("userRoles", userRoles);
         model.addAttribute("currentUser", new AppUser());
+        model.addAttribute("action","signin");
         return "login";
     }
-
 
     private String printErrors(ConstraintViolationException ce) {
         StringBuilder errorMsgString = new StringBuilder();
         for (ConstraintViolation violation : ce.getConstraintViolations()) {
-            errorMsgString.append(violation.getPropertyPath().toString().concat(" "));
-            errorMsgString.append(violation.getMessage().concat("; "));
+            String propertyStr = violation.getPropertyPath().toString().concat(" ");
+            errorMsgString.append(propertyStr.substring(0, 1).toUpperCase() + propertyStr.substring(1));
+            errorMsgString.append(violation.getMessage().concat("! "));
         }
         return errorMsgString.toString();
     }
