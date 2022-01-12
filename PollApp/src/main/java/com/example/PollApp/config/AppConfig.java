d3f133681959190e28771fa31aeda6f86d24a5c9
Hári Gábor <gabor.hari@ecobit.hu>
2022-01-12 12:47:06 +0100

    Minor messageSource config fix

--- PollApp/src/main/java/com/example/PollApp/config/AppConfig.java
+++ PollApp/src/main/java/com/example/PollApp/config/AppConfig.java
@@ -4,10 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 import javax.validation.Validation;
 import javax.validation.Validator;
-import java.util.Locale;
 
 @Configuration
 public class AppConfig implements WebMvcConfigurer {
@@ -22,7 +20,6 @@ public class AppConfig implements WebMvcConfigurer {
         var source = new ResourceBundleMessageSource();
         source.setBasenames("locale/messages");
         source.setUseCodeAsDefaultMessage(true);
-        source.setDefaultLocale(Locale.ENGLISH);
         return source;
     }
 }
